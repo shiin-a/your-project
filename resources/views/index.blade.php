fee84cad3a83f056f8648c1e0db9a36f56a77611
yusuke <shiinayusuke01@gmail.com>
2020-09-07 19:03:32 +0900

    コンポーネントを組み込む p92

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,16 @@
 
 @section('content')
 	<p>I want to go to US.</p>
+	
+	@component('components.message')
+		@slot('msg_title')
+		CAUSION!
+		@endslot
+
+		@slot('msg_content')
+			this is message
+		@endslot
+	@endcomponent
 @endsection
 
 
--- resources/views/layouts/helloapp.blade.php
+++ resources/views/layouts/helloapp.blade.php
@@ -1,25 +1,71 @@
-<!DOCTYPE html>
 <html>
+
 <head>
-<title>@yield('title')</title>
-<style>
-body{
-background-color:gray;
-}
-.content{
-background-color:blue;
-}
-</style>
+
+    <title>@yield('title')</title>
+
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <link href="http://localhost:8000/favicon.ico" type="image/x-icon" rel="shortcut icon">
+
+    <!-- <link rel="stylesheet" type="text/css" href="/css/app.css"> -->
+
+    <style>
+
+    body {font-size:16pt; color:#999; margin: 5px; }
+
+    h1 { font-size:50pt; text-align:right; color:#f6f6f6;
+
+        margin:-20px 0px -30px 0px; letter-spacing:-4pt; }
+
+    ul { font-size:12pt; }
+
+    th {background-color:#999; color:fff; padding:5px 10px; }
+
+    td {border: solid 1px #aaa; color:#999; padding:5px 10px; }
+
+    hr { margin: 25px 100px; border-top: 1px dashed #ddd; }
+
+    .menutitle {font-size:14pt; font-weight:bold; margin: 0px; }
+
+    .content {margin:10px; }
+
+    .footer { text-align:right; font-size:10pt; margin:10px;
+
+        border-bottom:solid 1px #ccc; color:#ccc; }
+
+    </style>
+
 </head>
+
 <body>
 
-<h1>@yield('title')</h1>
+    <h1>@yield('title')</h1>
 
-<p>My first paragraph.</p>
+    @section('menubar')
 
-<div class="content">
-@yield('content')
-</div>
+    <ul>
+
+        <p class="menutitle">menu</p>
+
+        <li>@show</li>
+
+    </ul>
+
+    <hr size="1">
+
+    <div class="content">
+
+    @yield('content')
+
+    </div>
+
+    <div class="footer">
+
+    @yield('footer')
+
+    </div>
+
 </body>
-</html>
 
+</html>
