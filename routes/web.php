921d6fec11761d800ffe00bb165f9ec1802937c7
yusuke <shiinayusuke01@gmail.com>
2020-09-11 23:59:47 +0900

    グローバルミドルウェアの登録、(Kernel.phpのにHelloMiddleware::classを追加。p118)

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('hello',  'HelloController@index')
-	->middleware(HelloMiddleware::class);
+Route::get('hello',  'HelloController@index');
 
 
